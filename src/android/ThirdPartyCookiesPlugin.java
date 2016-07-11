5b8a04dc94372391fee5db80a221634138c2487d
Jenkins <compilador@sankhya.com.br>
2016-07-11 10:39:12 -0300

    Atualizando plugin thirdpartycookies

--- src/android/ThirdPartyCookiesPlugin.java
+++ src/android/ThirdPartyCookiesPlugin.java
@@ -21,24 +21,24 @@ public class ThirdPartyCookiesPlugin extends CordovaPlugin {
     	((CordovaActivity)this.cordova.getActivity()).setIntent(intent);
     }
     
-    @TargetApi(12)
-    private void allowThirdPartyCookieCompat() {
-    	CookieManager.setAcceptFileSchemeCookies(true); 
-    }
-    
-	@TargetApi(21)
-	private void allowThirdPartyCookie(CordovaWebView webView) {
-        CookieManager cookieManager = CookieManager.getInstance();
-        cookieManager.setAcceptThirdPartyCookies(webView,true);
-	}
-    
-    @Override
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    	 //Allow third party cookies for Android Lollipop
-        if (Build.VERSION.SDK_INT >= 21) {
-        	allowThirdPartyCookie(webView);
-        }else if (Build.VERSION.SDK_INT >= 12) {
-        	allowThirdPartyCookieCompat();
-        }
-    }
+//    @TargetApi(12)
+//    private void allowThirdPartyCookieCompat() {
+//    	CookieManager.setAcceptFileSchemeCookies(true); 
+//    }
+//    
+//	@TargetApi(21)
+//	private void allowThirdPartyCookie(CordovaWebView webView) {
+//        CookieManager cookieManager = CookieManager.getInstance();
+//        cookieManager.setAcceptThirdPartyCookies(webView,true);
+//	}
+//    
+//    @Override
+//    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+//    	 //Allow third party cookies for Android Lollipop
+//        if (Build.VERSION.SDK_INT >= 21) {
+//        	allowThirdPartyCookie(webView);
+//        }else if (Build.VERSION.SDK_INT >= 12) {
+//        	allowThirdPartyCookieCompat();
+//        }
+//    }
 }
